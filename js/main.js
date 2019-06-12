b28ecdb447bc1c3a66e097782b5655ca886365ab
Andrew Kalashnikov <weezart@yandex.ru>
2019-06-12 19:18:36 +0300

    Task: #5. Вывод фото для галереи.

--- js/main.js
+++ js/main.js
@@ -13,13 +13,18 @@ var MIN_LIKES = 15;
 var MAX_LIKES = 200;
 var MIN_COMMENTS = 1;
 var MAX_COMMENTS = 2;
+var COUNT_PICTURES = 25;
+
+var pictureTemplate = document.querySelector('template').content.querySelector('.picture');
+var pictureFragment = document.createDocumentFragment();
+var pictures = document.querySelector('.pictures');
 
 var getImageUrl = function (filename) {
   return 'photos/' + filename + '.jpg';
 };
 
 var getRandomArbitary = function (min, max) {
-  return Math.floor(Math.random() * max + 1);
+  return Math.floor(Math.random() * (max - min) + min);
 };
 
 var getRandomValues = function (arr) {
@@ -35,11 +40,11 @@ var getRandomValues = function (arr) {
   return newArr.splice(0, randomCount);
 };
 
-var getDataPictures = function (i) {
+var getDataPictures = function (count) {
   var dataPictures = [];
-  for (var j = 0; j < i; j++) {
-    dataPictures[j] = {
-      'url': getImageUrl(j + 1),
+  for (var i = 0; i < count; i++) {
+    dataPictures[i] = {
+      'url': getImageUrl(i + 1),
       'likes': getRandomArbitary(MIN_LIKES, MAX_LIKES),
       'comments': getRandomValues(COMMENTS)
     };
@@ -48,4 +53,23 @@ var getDataPictures = function (i) {
   return dataPictures;
 };
 
-getDataPictures(25);
+var renderPicture = function (dataPicture) {
+  var picture = pictureTemplate.cloneNode(true);
+
+  picture.querySelector('.picture__img').src = dataPicture.url;
+  picture.querySelector('.picture__likes').textContent = dataPicture.likes;
+  picture.querySelector('.picture__comments').textContent = dataPicture.comments.length;
+
+  return picture;
+};
+
+var insertPictures = function (dataPictures) {
+  for (var i = 0; i < dataPictures.length; i++) {
+    pictureFragment.appendChild(renderPicture(dataPictures[i]));
+  }
+  pictures.appendChild(pictureFragment);
+};
+
+var dataPictures = getDataPictures(COUNT_PICTURES);
+
+insertPictures(dataPictures);
