5f622c5e6cd3092d01e98eb4c104b6f1da1b9ab9
Andrew Kalashnikov <weezart@yandex.ru>
2019-06-23 14:20:51 +0300

    Task: #10. Добавил перетаскивание на эффекты.

--- js/main.js
+++ js/main.js
@@ -149,28 +149,68 @@ var changeEffects = function () {
 
   for (var i = 0; i < effects.length; i++) {
     if (effects[i].checked) {
+      imageEffectLevelSlider.classList.remove('hidden');
+      var effectLevelValue = imageEffectDepth.offsetWidth / imageEffectLevel.offsetWidth;
+      imageEffectLevelInput.value = parseInt(effectLevelValue * 100, DEC);
       switch (effects[i].value) {
         case 'chrome':
-          previewImage.style.filter = 'grayscale(' + imageEffectPin.offsetLeft / imageEffectPin.parentNode.offsetWidth + ')';
+          previewImage.style.filter = 'grayscale(' + effectLevelValue + ')';
           break;
         case 'sepia':
-          previewImage.style.filter = 'sepia(' + imageEffectPin.offsetLeft / imageEffectPin.parentNode.offsetWidth + ')';
+          previewImage.style.filter = 'sepia(' + effectLevelValue + ')';
           break;
         case 'marvin':
-          previewImage.style.filter = 'invert(' + imageEffectPin.offsetLeft / imageEffectPin.parentNode.offsetWidth * 100 + '%)';
+          previewImage.style.filter = 'invert(' + effectLevelValue * 100 + '%)';
           break;
         case 'phobos':
-          previewImage.style.filter = 'blur(' + imageEffectPin.offsetLeft / imageEffectPin.parentNode.offsetWidth * 3 + 'px)';
+          previewImage.style.filter = 'blur(' + effectLevelValue * 3 + 'px)';
           break;
         case 'heat':
-          previewImage.style.filter = 'brightness(' + imageEffectPin.offsetLeft / imageEffectPin.parentNode.offsetWidth * 3 + ')';
+          previewImage.style.filter = 'brightness(' + effectLevelValue * 3 + ')';
           break;
         default:
+          imageEffectLevelSlider.classList.add('hidden');
           previewImage.style.removeProperty('filter');
           break;
       }
     }
   }
+};
+
+var onEffectPinChange = function (evt) {
+  evt.preventDefault();
+
+  var startCoords = evt.clientX;
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = startCoords - moveEvt.clientX;
+    startCoords = moveEvt.clientX;
+    var currentX = imageEffectLevelPin.offsetLeft - shift;
+    var validCoord = validateCoord(currentX);
+    imageEffectLevelPin.style.left = validCoord + 'px';
+
+    imageEffectDepth.style.width = validCoord / imageEffectLevel.offsetWidth * 100 + '%';
+    changeEffects();
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+};
+
+var validateCoord = function (x) {
+  if (x <= 0) {
+    x = 0;
+  } else if (x >= imageEffectLevel.offsetWidth) {
+    x = imageEffectLevel.offsetWidth;
+  }
+  return x;
 };
 
 var uploadPicture = function () {
@@ -208,11 +248,17 @@ var sizeDec = previewPicture.querySelector('.scale__control--smaller');
 var sizeInc = previewPicture.querySelector('.scale__control--bigger');
 var sizeValue = previewPicture.querySelector('.scale__control--value');
 var imageEffects = previewPicture.querySelector('.effects');
-var imageEffectPin = previewPicture.querySelector('.effect-level__pin');
+var imageEffectLevelSlider = previewPicture.querySelector('.effect-level');
+var imageEffectLevel = previewPicture.querySelector('.effect-level__line');
+var imageEffectLevelPin = previewPicture.querySelector('.effect-level__pin');
+var imageEffectDepth = previewPicture.querySelector('.effect-level__depth');
+var imageEffectLevelInput = previewPicture.querySelector('.effect-level__value');
 var uploadForm = document.querySelector('.img-upload__form');
 var uploadDescription = uploadForm.querySelector('.text__description');
+
 onUploadFormSubmit();
 uploadForm.action = UPLOAD_URL;
 
 insertPictures(dataPictures);
 pictureUpload.addEventListener('change', uploadPicture);
+imageEffectLevelPin.addEventListener('mousedown', onEffectPinChange);
